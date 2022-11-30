03a2fa7da3d4202fd10866e7d4b4d0b8b7ed63a5
harmanpunn <harmanpunn@gmail.com>
2022-11-30 18:04:35 -0500

    Removing redundant files

--- main.py
+++ main.py
@@ -13,11 +13,8 @@ from tqdm import tqdm
 import pygame
 
 from agents.p2.agent1 import Agent1
-from agents.p2.agent3 import Agent3
-from agents.p2.agent5 import Agent5
-from agents.p2.agent7 import Agent7
 
-from agents.p3.p3Agent1 import P3Agent1
+from agents.p2.agent7 import Agent7
 
 import numpy as np
 
@@ -94,46 +91,10 @@ def runGame(graph : Graph):
     prey = Prey(graph)
     predator = Predator(graph)
 
-    if not Environment.getInstance().p3:
-        if Environment.getInstance().agent % 2 == 0:
-            Environment.getInstance().careful = True
-
-        if Environment.getInstance().agent < 3:
-            agent : GraphEntity = Agent1(graph)
-        elif Environment.getInstance().agent < 5:
-            agent : GraphEntity = Agent3(graph) 
-        elif Environment.getInstance().agent < 7:
-            agent : GraphEntity = Agent5(graph) 
-            agent.belief = [1.0 if i==predator.getPosition() else 0.0 for i in range(0,Environment.getInstance().node_count)]
-        else:
-            agent : GraphEntity = Agent7(graph) 
-            agent.predator_belief = [1.0 if i==predator.getPosition() else 0.0 for i in range(0,Environment.getInstance().node_count)]        
-
-            # agent : GraphEntity = get_class("Agent"+str(Environment.getInstance().agent))(graph)
-
-        if Environment.getInstance().agent==9:
-            Environment.getInstance().noisy_agent = True
-            Environment.getInstance().noisy = True
-            Environment.getInstance().careful = True
-
-        if Environment.getInstance().agent==10:
-            Environment.getInstance().noisy = False
-            Environment.getInstance().noisy_agent = False
-            Environment.getInstance().careful = True
-            Environment.getInstance().agentX = True
-    else:
-        if Environment.getInstance().agent==1:
-            agent : GraphEntity = P3Agent1(graph)
+    agent : GraphEntity = Agent7(graph) 
+    agent.predator_belief = [1.0 if i==predator.getPosition() else 0.0 for i in range(0,Environment.getInstance().node_count)]        
 
     running = 1
-
-    if Environment.getInstance().noisy:
-        print("So NOISY!")
-    
-    if Environment.getInstance().careful:
-        print("TipToe B)")
-
-    knownRounds = None
     while True:
         if Environment.getInstance().ui:
             sleep(0.2)
@@ -144,34 +105,22 @@ def runGame(graph : Graph):
             graph.surveyed = False
 
             info = {}
-            if not Environment.getInstance().p3:
-                if Environment.getInstance().agent<3:
-                    info = {
-                        'prey' : prey.getPosition(),
-                        'predator' : predator.getPosition()
-                    }
-                elif Environment.getInstance().agent<5:
-                    info = {
-                        'predator' : predator.getPosition()
-                    }
-                elif Environment.getInstance().agent<7:
-                    info = {
-                        'prey' : prey.getPosition()
-                    }
-            else:
-                if Environment.getInstance().agent ==1:
-                    info = {
-                        'prey' : prey.getPosition(),
-                        'predator' : predator.getPosition()
-                    }
+            # if Environment.getInstance().agent<3:
+            #     info = {
+            #         'prey' : prey.getPosition(),
+            #         'predator' : predator.getPosition()
+            #     }
+            # elif Environment.getInstance().agent<5:
+            #     info = {
+            #         'predator' : predator.getPosition()
+            #     }
+            # elif Environment.getInstance().agent<7:
+            #     info = {
+            #         'prey' : prey.getPosition()
+            #     }
 
             graph.node_states_blocked= True
             knows = agent.__update__(graph, info)
-
-            if knownRounds!=None:
-                knownRounds = [knows[i]+knownRounds[i] for i in range(0,len(knownRounds))]
-            else:
-                knownRounds = knows
                 
             graph.node_states_blocked = False
             
@@ -202,10 +151,9 @@ def runGame(graph : Graph):
                 sleep(2)
             break
     graph.reset_states()    
-    knownRounds = [k/step_count for k in knownRounds]
     if Environment.getInstance().ui:
         pygame.quit()
-    return [step_count, game_state, knownRounds]  
+    return [step_count, game_state]  
 
 def collectData() -> None:
     stats_dict = dict()
@@ -227,7 +175,6 @@ def collectData() -> None:
         confidencePerGraph = [x/Environment.getInstance().games for x in confidencePerGraph]
         for i in range(0,len(confidencePerGraph)):
             totalConfidences[i].append(confidencePerGraph[i])
-        
     
     for k in step_count_list:
         step_count_list[k] /= Environment.getInstance().games * Environment.getInstance().graphs
@@ -235,8 +182,6 @@ def collectData() -> None:
     for t in totalConfidences:
         t = np.array(t)
         
-    
-
     win_count = game_state_list.count(1)
     lose_count = game_state_list.count(0)
     timeout_count = game_state_list.count(-1)
@@ -253,17 +198,6 @@ def collectData() -> None:
     print("Timeout Step Count: ",step_count_list[-1])
     print("Timeout %: ", (timeout_count/z) * 100)
     print("================================")
-    '''    
-    stats_dict['graph_type'] = 1
-    stats_dict['step_count'] = step_count
-    stats_dict['game_state'] = game_state
-    '''
-    # print(stats_dict) 
-    # stats_df = pd.DataFrame(columns=['Graph Type', 'Step Count', 'Game State'])
-    # stats_df.loc[len(stats_df.index)] = []
-    # stats_df.loc[len()]
-    # stats_df = pd.DataFrame(data = stats_dict)
-    # stats_df.to_csv('Agent'+str(Environment.getInstance().agent)+'.csv', index=False)
     pass
 
 
